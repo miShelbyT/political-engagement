83f4137cfb554cd57d9a7339b896827935714398
ChippyChoppy <rebeccamargaret13@gmail.com>
2022-02-13 17:00:21 -0500

    sunday 5pm

--- src/apis/callCongress.js
+++ src/apis/callCongress.js
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from 'axios'
 
 const KEY = '5Ne4FFTyA8G33s8Ukq8I1c9QiOmwCYcjNd4xCvRE'
 
 export default axios.create({
-    baseURL: 'https://api.propublica.org/congress/v1/117',
-    params: {
-        chamber: '',
-
-    }
+  baseURL: 'https://api.propublica.org/congress/v1/117',
+  headers: {
+    key: KEY,
+    : 'Access-Control-Allow-Origin': '*',
+  },
 })
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
+import axios from 'axios'
 import React from 'react'
 import callCongress from '../apis/callCongress'
+import SearchBar from './SearchBar'
 
 class App extends React.Component {
   state = {
@@ -8,10 +10,28 @@ class App extends React.Component {
     selectedSen: null,
     selectedRep: null,
   }
+  componentDidMount() {
+    callCongress.get('/senate/members.json', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+    .then(res => {
+      console.log(res)
+
+    })
+  }
+  // onTermSubmit = async (term) => {
+  //   const response = await callCongress.get('/senate/members.json')
+  //   .then
+  // }
   render() {
     return (
       <div>
         <h1>Call Congress</h1>
+        <h2>(202) 224-3121</h2>
+        <SearchBar />
+        
       </div>
     )
   }
