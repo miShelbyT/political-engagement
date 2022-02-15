c20f32f751487005491412284aae108b837c9eae
miShelbyT <mi.shelbyrose@gmail.com>
2022-02-14 19:50:20 -0500

    inserted a tag using search results

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react'
 import instance from '../apis/callCongress'
 import SearchBar from './SearchBar'
 
+///Users/michelletalbert/Development/code/call-congress/apis/callCongress.js
+
 function App () {
 
   const [senators, setSenators] = useState([])
@@ -14,6 +16,7 @@ function App () {
 
 
   const [selectedSen, setSelectedSen] = useState(null)
+  console.log(selectedSen)
   const [selectedRep, setSelectedRep] = useState(null)
   
   
@@ -46,7 +49,7 @@ function App () {
         <h1>Call Congress</h1>
         <h2>(202) 224-3121</h2>
         <SearchBar searchTerm={senatorSearchTerm} setSearchTerm={setSenatorSearchTerm} setSelectedRep={setSelectedSen} repsList={senators}/>
-       {/* {selectedSen ?  <div>{selectedSen}</div> : null} */}
+       {selectedSen ?  <a href={selectedSen[0].contact_form}>{selectedSen[0].first_name} {selectedSen[0].last_name}</a> : null}
       </div>
     )
   }
