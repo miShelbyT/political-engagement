b8f9a97ab8745e9bc2380f60ed50b350d44e24c4
miShelbyT <mi.shelbyrose@gmail.com>
2022-02-22 13:49:10 -0500

    added new ViewContainer component, everything seems to be functioning.......!

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import { useState, useEffect } from 'react'
 import apiCall from '../apis/callCongress'
 import Header from './Header'
-import IndividualInfo from './IndividualInfo'
-import SearchContainer from './SearchContainer'
+import ViewContainer from './ViewContainer'
+
 import styled from 'styled-components'
 
 function App() {
   const [renderSearchButton, setRenderSearchButton] = useState(true)
   const [senators, setSenators] = useState([])
   const [reps, setReps] = useState([])
-  const [selectedSen, setSelectedSen] = useState([])
-  const [selectedRep, setSelectedRep] = useState([])
 
   useEffect(() => {
     apiCall
@@ -34,49 +32,15 @@ function App() {
       })
   }, [])
 
-  // function renderIndivResults() {
-  //   if(selectedSen) {
-  //     return <IndividualInfo selectedCon={selectedSen} />
-  //   } else if(selectedRep) {
-  //     return <IndividualInfo selectedCon={selectedRep} />
-  //   }
-  // }
-
-  // onTermSubmit = async (term) => {
-  //   const response = await callCongress.get('/senate/members.json')
-  //   .then
-  // }
-function setCon() {
-  // console.log(selectedSen, selectedRep)
-  if(selectedSen.length !== 0) {
-    console.log(selectedSen)
-    return selectedSen
-  } else if(selectedRep.length !== 0) {
-    console.log(selectedRep)
-    return selectedRep
-  }
-}
-
   return (
     <Page>
       <Header
         renderSearchButton={renderSearchButton}
         setRenderSearchButton={setRenderSearchButton}
       />
-      {senators && reps  ? <>
-      <SearchContainer
-        setSelectedSen={setSelectedSen}
+      <ViewContainer 
         senators={senators}
-        setSelectedRep={setSelectedRep}
-        reps={reps}
-        selectedSen={selectedSen}
-        selectedRep={selectedRep}
-        param="last_name"
-      />
-      </> : <div>Please hold....</div> }
-
-      {selectedSen.length || selectedRep.length ? <IndividualInfo selectedCon={setCon()}/> : null }
- 
+        reps={reps}/>
     </Page>
   )
 }
